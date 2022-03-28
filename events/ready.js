8f41b6cc57225bf8aff9fe89c5de0e3418aaf209
Dreamyplayer <saikumarchinna123@gmail.com>
2022-03-29 02:59:30 +0530

    Ready event updated

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,51 @@
 export const name = 'ready';
 export const once = true;
-export function execute(client) {
-  let guilds = client.guilds.cache.map(g => g.name);
-  // let randomServer = guilds[Math.floor(guilds.length * Math.random())];
+import { Anime, Music } from '../collection.js';
+import { randomNoRepeats } from '../utils/functions.js';
 
-  // setInterval(async () => {
-  client.user
-    .setPresence({
-      status: 'Online',
-      activity: {
-        name: guilds,
-        type: 'WATCHING',
-        url: 'https://discord.gg/V8guWsR',
-      },
-    })
-    .then(console.log)
-    .catch(console.error);
-  // }, 3600000); // 1 Hour
+export function execute(client) {
+  let intervalId;
+  setInterval(() => {
+    const type = ['WATCHING', 'LISTENING'];
+    let ranType = randomNoRepeats(type)();
+
+    console.log(ranType);
+    clearInterval(intervalId);
+
+    setTimeout(() => {
+      if (ranType === 'WATCHING') {
+        intervalId = setInterval(async () => {
+          let ranAnime = randomNoRepeats(Anime)();
+          await client.user.setPresence({
+            status: 'dnd',
+            activities: [
+              {
+                name: `📽️ ${ranAnime}`,
+                type: 'WATCHING',
+                url: 'https://discord.gg/V8guWsR',
+              },
+            ],
+          });
+          console.log(`Watching ${ranAnime}`);
+        }, 1800000); // 30 minutes
+      } else {
+        intervalId = setInterval(async () => {
+          let ranMusic = randomNoRepeats(Music)();
+          await client.user.setPresence({
+            status: 'dnd',
+            activities: [
+              {
+                name: `🎧 ${ranMusic}`,
+                type: 'LISTENING',
+                url: 'https://discord.gg/V8guWsR',
+              },
+            ],
+          });
+          console.log(`Listening to ${ranMusic}`);
+        }, 600000); // 10 minutes
+      }
+    });
+  }, 43200000); // 12 hours
 
   console.log(`Ready! Logged in as ${client.user.tag}`);
 }
