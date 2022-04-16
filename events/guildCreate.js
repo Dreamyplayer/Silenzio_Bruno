0d71610982751570b12c9c68b9d9702b6787199f
Dreamyplayer <saikumarchinna123@gmail.com>
2022-04-16 14:20:38 +0530

    build(deps): update 1 file, delete 1 file and create 1 file

--- events/guildCreate.js
+++
@@ -1,5 +0,0 @@
-export const name = 'guildCreate';
-
-export async function execute(guild) {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-}
